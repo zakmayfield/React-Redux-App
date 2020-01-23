bfb7f3b552f42657f7107b732e151cd1a13d52fb
Zachary Mayfield <zakmayfield@gmail.com>
2020-01-23 17:35:01 -0500

    MVP reached -- styling is meh for now, if i go back to work on this project i'll play around with the styling as of right now its fine.

--- dnd-app/src/components/MonsterCard.js
+++ dnd-app/src/components/MonsterCard.js
@@ -8,16 +8,35 @@ const MonsterCard = (props) => {
         return (
           <div key={item.name} className="card" >
             <h3 className="name" >{item.name}</h3>
+
+            <div className="alignmentDetails">
+              
+              {item.alignment === 'chaotic evil' ? 
+                <p className="alignment">{item.alignment}***</p>
+              : 
+                <p className="alignment">{item.alignment}</p>}
+            </div>
+
             {item.img_main ?
               <img className="img" src={item.img_main} />
             :
-              <img src={monsterImg} />}
-            <p className="type">{item.type}</p>
-            <p className="size">{item.size}</p>
-            <p className="alignment">Alignment: {item.alignment}</p>
+              <img className="img" src={monsterImg} />}
+
+            <div className="physicalDetails">
+              <p className="type">{item.type}</p>
+              <p className="size">{item.size}</p>
+            </div>
+
             <div className="stats">
               <span className="ac">AC: {item.armor_class}</span>
               <span className="hp">HP: {item.hit_points}</span>
+            </div>
+
+            <div className="additionalDetails">
+              <p className="language"><span className="languageTitle">Languages:</span> {item.languages}</p>
+              <div><span className="actionTitle">Actions: </span>{item.actions.map(item => {
+                return <p className="actionName">{item.name}</p>
+              })}</div>
             </div>
           </div>
         )
